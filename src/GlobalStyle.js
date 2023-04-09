397e79da77a326d70cdd57934af024f57ebf395d
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-04-09 22:59:56 +0300

    add style for fonts

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,13 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
+
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     background-color: #AED7FC;
     color: #171717;
     font-size: 22px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: -apple-system, BlinkMacSystemFont, 'Poppins', 'Montserrat', 'Segoe UI', 'Roboto', 'Oxygen', 
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -16,6 +17,14 @@ export const GlobalStyle = createGlobalStyle`
   h1, h2 {
     color: #141414;
     text-align: center;
+    font-family: 'Poppins', url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+    font-style: normal;
+    font-weight: 400;
+    font-size: 48px;
+    line-height: 72px;
+  }
+  h2{
+    font-size: 40px;
   }
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
