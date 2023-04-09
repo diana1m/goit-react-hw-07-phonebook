72029e68f0d6a521eb72ccac683dd26a02c4953b
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-04-09 22:40:42 +0300

    add API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from 'components/App';
-import { Loader } from 'components/Loader/Loader';
+import { App } from 'components/App/App';
 
 import { Provider } from "react-redux";
-import { store, persistor } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<Loader/>} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,27 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; 
  
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts']
-}
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+//   whitelist: ['contacts']
+// }
  
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+// });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer
+    reducer: {
+      contacts: contactsReducer,
+      filter: filterReducer,
+    }
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
